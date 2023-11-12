3f6a54a9e5e82203cd4b12d473011ab2ec5906d0
pmoreira <pmoreira@firenzze.com>
2023-11-12 13:29:25 -0300

    chore(eslint): use vue3-recommended plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,15 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  rules: {
+    'vue/new-line-between-multi-line-property': 'error',
+},
 }
