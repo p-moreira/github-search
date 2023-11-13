9540be4933fcaf1180b5661d29993713c3597a10
pmoreira <pmoreira@firenzze.com>
2023-11-12 21:32:05 -0300

    style(prettier): format eslint config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,12 +2,18 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': ['plugin:vue/vue3-recommended', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier/skip-formatting', 'plugin:storybook/recommended'],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  rules: {
-    'vue/new-line-between-multi-line-property': 'error',
-},
+    root: true,
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting',
+        'plugin:storybook/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        'vue/new-line-between-multi-line-property': 'error',
+    },
 }
