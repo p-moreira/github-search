355b5574a01f9ae5d27671ec48415f0574f928f5
pmoreira <pmoreira@firenzze.com>
2023-11-18 15:31:55 -0300

    chore: add custom tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('./src/assets/design-system/build/css/tw-extend/color')
+
 module.exports = {
     content: ['./src/**/*.{vue,js,jsx,ts,tsx}', './public/index.html'],
     theme: {
+        screens: {
+            sm: '600px',
+            md: '900px',
+            lg: '1200px',
+            xl: '1600px',
+        },
+        colors,
         extend: {},
     },
     plugins: [],
