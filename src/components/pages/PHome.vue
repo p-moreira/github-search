9de9dcaf8d20382e4af3bf441f2ac5c93bfbb5b2
pmoreira <pmoreira@firenzze.com>
2023-11-18 23:21:50 -0300

    feat(p-home): add ds variables setting custom css resets

--- src/components/pages/PHome.vue
+++ src/components/pages/PHome.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-    <main>
-        <h1>Main content</h1>
+    <main class="m-ds-spacing-3xl">
+        <h1 class="text-ds-font-headline-large-font-size text-ds-color-primary">Main content</h1>
+        <p class="text-ds-font-body-large-font-size leading-ds-ref-line-height-4 mt-ds-spacing-md">
+            This content is using the custom Design System classes!
+        </p>
     </main>
 </template>
