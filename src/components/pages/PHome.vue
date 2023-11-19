d17ea9bf396ed3f3b9d0d9cdaf4e577edb22994a
pmoreira <pmoreira@firenzze.com>
2023-11-19 00:24:55 -0300

    feat: allow changing the app theme

--- src/components/pages/PHome.vue
+++ src/components/pages/PHome.vue
@@ -1,10 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const theme = ref('light')
+const handleInputThemeChange = (event: Event) => {
+    theme.value = (event.target as HTMLInputElement).checked ? 'dark' : 'light'
+    document.body.setAttribute('data-theme', theme.value)
+}
+</script>
 
 <template>
     <main class="m-ds-spacing-3xl">
         <h1 class="text-ds-font-headline-large-font-size text-ds-color-primary">Main content</h1>
-        <p class="text-ds-font-body-large-font-size leading-ds-ref-line-height-4 mt-ds-spacing-md">
+
+        <p class="mt-ds-spacing-md text-ds-font-body-large-font-size leading-ds-ref-line-height-4">
             This content is using the custom Design System classes!
         </p>
+
+        <form class="mt-ds-spacing-md flex gap-ds-spacing-sm">
+            <input
+                id="theme"
+                type="checkbox"
+                name="theme"
+                :value="theme"
+                @change="handleInputThemeChange"
+            />
+            <label for="theme">Dark Mode</label>
+        </form>
     </main>
 </template>
