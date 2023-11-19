bc9e9ea7fc0008b9f9a6a85da8512e602cef72f3
pmoreira <pmoreira@firenzze.com>
2023-11-19 17:13:06 -0300

    refactor(tailwind config): replace require by import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require('./src/assets/design-system/build/tailwind/color')
-const spacing = require('./src/assets/design-system/build/tailwind/spacing')
-const fontFamily = require('./src/assets/design-system/build/tailwind/fontFamilies')
-const fontSize = require('./src/assets/design-system/build/tailwind/fontSizes')
-const fontWeight = require('./src/assets/design-system/build/tailwind/fontWeights')
-const lineHeight = require('./src/assets/design-system/build/tailwind/lineHeights')
-const letterSpacing = require('./src/assets/design-system/build/tailwind/letterSpacing')
-const textTransform = require('./src/assets/design-system/build/tailwind/textCase')
-const textDecoration = require('./src/assets/design-system/build/tailwind/textDecoration')
+import colors from './src/assets/design-system/build/tailwind/color'
+import spacing from './src/assets/design-system/build/tailwind/spacing'
+import fontFamily from './src/assets/design-system/build/tailwind/fontFamilies'
+import fontSize from './src/assets/design-system/build/tailwind/fontSizes'
+import fontWeight from './src/assets/design-system/build/tailwind/fontWeights'
+import lineHeight from './src/assets/design-system/build/tailwind/lineHeights'
+import letterSpacing from './src/assets/design-system/build/tailwind/letterSpacing'
+import textTransform from './src/assets/design-system/build/tailwind/textCase'
+import textDecoration from './src/assets/design-system/build/tailwind/textDecoration'
+import borderRadius from './src/assets/design-system/build/tailwind/borderRadius'
+import opacity from './src/assets/design-system/build/tailwind/opacity'
 
-module.exports = {
+export default {
     content: ['./src/**/*.{vue,js,jsx,ts,tsx}', './public/index.html'],
     theme: {
         screens: {
@@ -29,6 +31,8 @@ module.exports = {
             letterSpacing,
             textTransform,
             textDecoration,
+            borderRadius,
+            opacity,
         },
     },
     plugins: [],
