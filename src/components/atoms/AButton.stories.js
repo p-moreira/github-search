26093c0e3875d75713f06b01f5aa45b4afe301ad
pmoreira <pmoreira@firenzze.com>
2023-11-19 23:29:37 -0300

    doc(a-button): add a storybook story

---
+++ src/components/atoms/AButton.stories.js
@@ -0,0 +1,13 @@
+import AButton from './AButton.vue'
+
+const meta = {
+    title: 'Components/AButton',
+    component: AButton,
+    tags: ['autodocs'],
+}
+
+export default meta
+
+export const Default = {
+    args: {},
+}
