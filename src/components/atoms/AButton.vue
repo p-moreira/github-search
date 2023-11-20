31aa852c03219914a6ff3c85872e44ff805f783c
pmoreira <pmoreira@firenzze.com>
2023-11-20 18:47:21 -0300

    feat(a-button): add styles

--- src/components/atoms/AButton.vue
+++ src/components/atoms/AButton.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-    <button>Button</button>
+    <button
+        class="relative flex cursor-pointer justify-center rounded-ds-radii-xs border-0 bg-ds-color-primary px-ds-spacing-md py-ds-spacing-sm font-ds-font-label-large-font-family text-ds-font-label-large-font-size font-ds-font-label-large-font-weight text-ds-color-on-primary transition-all duration-300"
+    >
+        Button
+    </button>
 </template>
