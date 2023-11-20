140180608ae14b4409020ada63a1fcf86d9e4c67
pmoreira <pmoreira@firenzze.com>
2023-11-19 23:27:56 -0300

    feat(a-button): add a label text

--- src/components/atoms/AButton.test.ts
+++ src/components/atoms/AButton.test.ts
@@ -1,3 +1,20 @@
-import { describe } from 'vitest'
+import '@testing-library/jest-dom'
+import { describe, expect, it } from 'vitest'
+import { render } from '@testing-library/vue'
+import AButton from './AButton.vue'
 
-describe('AButton', () => {})
+describe('AButton', () => {
+    it('renders the button', () => {
+        const { getByRole } = render(AButton)
+        const button = getByRole('button')
+
+        expect(button).toBeInTheDocument()
+    })
+
+    it('renders the button label', () => {
+        const { getByRole } = render(AButton)
+        const button = getByRole('button')
+
+        expect(button).toHaveTextContent('Button')
+    })
+})
