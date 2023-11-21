7700ebea3faf68ccaf19cb52a39055fa79b832ea
pmoreira <pmoreira@firenzze.com>
2023-11-20 23:57:43 -0300

    docs(a-button): add label prop and click event

--- src/components/atoms/AButton.stories.js
+++ src/components/atoms/AButton.stories.js
@@ -4,12 +4,23 @@ const meta = {
     title: 'Components/AButton',
     component: AButton,
     tags: ['autodocs'],
+    argTypes: {
+        onClick: {
+            action: 'clicked',
+            table: { disable: true },
+        },
+        click: {
+            description: 'click event',
+            table: { type: { summary: '-' } },
+        },
+    },
 }
 
 export default meta
 
 export const Default = {
     args: {
+        label: 'Button',
         disabled: false,
     },
 }
