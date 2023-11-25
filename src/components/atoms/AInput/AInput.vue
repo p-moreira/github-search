9f21ea4b2b43064c21de33f127c8fac6b5e01d99
pmoreira <pmoreira@firenzze.com>
2023-11-25 16:33:46 -0300

    feat(a-input): add basic styling

--- src/components/atoms/AInput/AInput.vue
+++ src/components/atoms/AInput/AInput.vue
@@ -2,6 +2,8 @@
 
 <template>
     <div>
-        <input />
+        <input
+            class="rounded-ds-radii-xs border border-ds-color-outline bg-ds-color-surface px-ds-ref-spacing-4 py-ds-ref-spacing-3 text-ds-font-body-large-font-size font-ds-font-body-large-font-weight leading-ds-font-body-large-line-height tracking-ds-font-body-large-letter-spacing text-ds-color-on-surface placeholder-ds-color-on-surface placeholder-opacity-ds-opacity-color-medium transition-all duration-300"
+        />
     </div>
 </template>
