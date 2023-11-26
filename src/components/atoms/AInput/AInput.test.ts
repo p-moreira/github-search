8d251a419b60df7fd6eb4abcfaf0ef9ed956b976
pmoreira <pmoreira@firenzze.com>
2023-11-25 22:00:35 -0300

    feat(a-input): emit the input event

--- src/components/atoms/AInput/AInput.test.ts
+++ src/components/atoms/AInput/AInput.test.ts
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 import { describe, it, expect } from 'vitest'
-import { render, screen } from '@testing-library/vue'
+import { fireEvent, render, screen } from '@testing-library/vue'
 import AInput from './AInput.vue'
 
 describe('AInput', () => {
@@ -9,5 +9,28 @@ describe('AInput', () => {
         const input = getByRole('textbox')
 
         expect(input).toBeInTheDocument()
+    })
+
+    it('emits the right events when updating the input text', async () => {
+        const { getByRole, emitted } = render(AInput)
+        const input = getByRole('textbox')
+
+        expect(input).toHaveValue('')
+
+        await fireEvent.update(input, 'test')
+
+        expect(input).toHaveValue('test')
+
+        // Event name
+        expect(emitted()).toHaveProperty('input')
+        expect(emitted()).toHaveProperty('update:modelValue')
+
+        // Event count
+        expect(emitted().input).toHaveLength(1)
+        expect(emitted()['update:modelValue']).toHaveLength(1)
+
+        // Event payload
+        expect(emitted().input[0]).toHaveLength(1)
+        expect(emitted()['update:modelValue'][0]).toHaveLength(1)
     })
 })
--- src/components/atoms/AInput/AInput.vue
+++ src/components/atoms/AInput/AInput.vue
@@ -1,9 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = withDefaults(
+    defineProps<{
+        modelValue?: string
+    }>(),
+    {
+        modelValue: '',
+    }
+)
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: string): void
+}>()
+
+const handleInput = () => {
+    emit('update:modelValue', props.modelValue)
+}
+</script>
 
 <template>
     <div>
         <input
             class="focus-visible:border-ds- rounded-ds-radii-xs border border-ds-color-outline bg-ds-color-surface px-ds-ref-spacing-4 py-ds-ref-spacing-3 text-ds-font-body-large-font-size font-ds-font-body-large-font-weight leading-ds-font-body-large-line-height tracking-ds-font-body-large-letter-spacing text-ds-color-on-surface placeholder-ds-color-on-surface placeholder-opacity-ds-opacity-color-medium transition-all duration-300 focus-visible:outline-ds-color-on-surface"
+            :value="modelValue"
+            @input="handleInput"
         />
     </div>
 </template>
