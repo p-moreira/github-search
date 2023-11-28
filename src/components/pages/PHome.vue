2a75337be6237b50b2944500c182347aca2901a8
pmoreira <pmoreira@firenzze.com>
2023-11-28 11:25:58 -0300

    feat(p-home): integrate with the user store

--- src/components/pages/PHome.vue
+++ src/components/pages/PHome.vue
@@ -1,30 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed } from 'vue'
+import OHeaderMain from '../organisms/OHeaderMain/OHeaderMain.vue'
+import { useUserStore } from '@/stores/user'
 
-const theme = ref('light')
-const handleInputThemeChange = (event: Event) => {
-    theme.value = (event.target as HTMLInputElement).checked ? 'dark' : 'light'
-    document.body.setAttribute('data-theme', theme.value)
-}
+const userStore = useUserStore()
+const users = computed(() => userStore.users)
 </script>
 
 <template>
-    <main class="m-ds-spacing-3xl">
-        <h1 class="text-ds-font-headline-large-font-size text-ds-color-primary">Main content</h1>
-
-        <p class="mt-ds-spacing-md text-ds-font-body-large-font-size leading-ds-ref-line-height-4">
-            This content is using the custom Design System classes!
-        </p>
-
-        <form class="mt-ds-spacing-md flex gap-ds-spacing-sm">
-            <input
-                id="theme"
-                type="checkbox"
-                name="theme"
-                :value="theme"
-                @change="handleInputThemeChange"
-            />
-            <label for="theme">Dark Mode</label>
-        </form>
-    </main>
+    <OHeaderMain />
+    <main class="pt-ds-spacing-5xl">{{ users }}</main>
 </template>
